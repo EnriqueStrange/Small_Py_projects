867a4225c496be24107279350bc4d82c4eaa19d4
Mr.Strange <39236761+EnriqueStrange@users.noreply.github.com>
2023-02-02 19:08:54 +0530

    Update text_generator.py

--- text_generator.py
+++ text_generator.py
@@ -1,10 +1,19 @@
 import openai
 
-openai.api_key = "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"
+# Replace "YOUR_API_KEY" with your OpenAI API key
+openai.api_key = "YOUR_API_KEY"
 
 def generate_text():
+    """
+    This function generates random meaningful text using OpenAI GPT-3 API
+    """
+    # Define the prompt or the text to be used as context
     prompt = "Generate a random meaningful sentence"
+
+    # Generate the text using OpenAI API
     generated_text = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=1024, n=1, temperature=0.5)["choices"][0]["text"]
+
+    # print the generated text
     print(generated_text)
 
 generate_text()
